846945f193abb2b0ffff432f10074970b93e86ac
Yotam Gingold <yotam@yotamgingold.com>
2025-09-09 23:33:00 -0400

    demo/chrono_sleep_for.cpp runs in a loop

--- demo/chrono_sleep_for.cpp
+++ demo/chrono_sleep_for.cpp
@@ -4,16 +4,19 @@
 
 using namespace std;
 int main(int argc, char *argv[]) {
-    const auto t1 = std::chrono::steady_clock::now();
-    // Do some work
-    // ...
-    const auto t2 = std::chrono::steady_clock::now();
-    
-    // Our desired total time.
-    double h = .5;
-    
-    // Sleep for the difference.
-    cout << "About to sleep\n";
-    std::this_thread::sleep_for( std::chrono::duration<double>(h) - (t2-t1) );
-    cout << "Done\n";
+    while( true ) {
+        const auto t1 = std::chrono::steady_clock::now();
+        // Do some work
+        // ...
+        std::cout << "It's now!\n";
+        const auto t2 = std::chrono::steady_clock::now();
+        
+        // Our desired total time.
+        double h = 2.0;
+        
+        // Sleep for the difference.
+        cout << "About to sleep\n";
+        std::this_thread::sleep_for( std::chrono::duration<double>(h) - (t2-t1) );
+        cout << "Done\n";
+    }
 }
